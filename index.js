2f4f5f2a5b33ce7d90c10d6e6dd21f25df257118
Dipesh <dipeshkaflen2060@gmail.com>
2024-07-31 23:30:28 +0530

    Structured code

--- index.js
+++ index.js
@@ -4,52 +4,66 @@ const start=document.getElementById('start');
 const pause=document.getElementById('pause');
 const reset=document.getElementById('reset');
 const span=document.querySelector("#display");
-//span.innerHTML="Hello";
-let flag=false;
-input.addEventListener('change',()=>{
-    progress.setAttribute('max',input.value*60);
-    progress.setAttribute('value',0);
-    flag=false;
-})
-let interval;
+let currTime=0;
+let maxTime=0;
+let IntervalId;
+let minutes;
+let sec=59;
+let isnewStart=true;
+
+function initializeValues(){
+    if(isnewStart){
+     currTime=0;
+    
+     maxTime=parseFloat(input.value);
+    // console.log(maxTime);
+     minutes=parseInt(maxTime);
+    maxTime=maxTime*60;
+    sec=60;
+    if(maxTime%60!=0){
+        sec=maxTime%60;
+    }else{
+        minutes=minutes-1;
+    }
+
+    progress.value=0;
+    progress.setAttribute('max',maxTime);
+    isnewStart=false;
+}
+}
+
+
+function displayRemainingTime(){        
+    if(sec==0){
+        minutes=minutes-1;
+        sec=59;
+    }
+    sec=sec-1;
+    span.innerText=`${minutes}:${sec}`;    
+
+}
+
+
+
+
+function increaseCounter(){    
+        currTime++;     
+        progress.value=progress.value+1;
+        displayRemainingTime();
+        if(currTime==maxTime){
+           clearInterval(IntervalId);
+           isnewStart=true;
+        }      
+}
+
 
 start.addEventListener('click',()=>{
-    // console.log(typeof input.value);
-    if(input.value==0 || input.value==undefined)
-        alert('Please enter time before starting');
-    let num;
-    if(flag==false){
-     num=input.value*60;
-    }else{
-        num=input.value;
-    }
-    // console.log(typeof num);
-    let min=input.value-1;
-    let sec=60;
-  interval=setInterval(function(){    
-       progress.value=progress.value+1;       
-       if(progress.value==progress.max){
-        clearInterval(interval);
-        
-       }   
-        if(progress.value%60==0){
-            min=min-1;
-            sec=60;
-        }
-        sec=sec-1; 
-      
-       span.innerHTML=`${min}:${sec}`;
-    },1000)
+    initializeValues();
+   IntervalId=setInterval(increaseCounter,1000)
+   
 })
 
-reset.addEventListener('click',()=>{
-    input.value=0;
-    progress.value=0;
-    progress.max=0;
-    span.innerHTML="";
-    clearInterval(interval);
-})
-pause.addEventListener('click',()=>{
-    clearInterval(interval);
-    flag=true;
+pause.addEventListener("click",()=>{
+    clearInterval(IntervalId);
+     isnewStart=false;
 })
