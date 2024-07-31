88db3c82886a206d9cde939f67f237377785be30
Dipesh <dipeshkaflen2060@gmail.com>
2024-07-31 23:49:24 +0530

    final functionality Added!

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const start=document.getElementById('start');
 const pause=document.getElementById('pause');
 const reset=document.getElementById('reset');
 const span=document.querySelector("#display");
-let currTime=0;
-let maxTime=0;
+
+let currTime;
+let maxTime;
 let IntervalId;
 let minutes;
-let sec=59;
+let sec;
 let isnewStart=true;
 
-function initializeValues(){
-    if(isnewStart){
-     currTime=0;
-    
-     maxTime=parseFloat(input.value);
-    // console.log(maxTime);
+
+//function for initializing values
+function initializeValues(){      
+ if(isnewStart){
+     currTime=0;    
+     maxTime=parseFloat(input.value);    
      minutes=parseInt(maxTime);
     maxTime=maxTime*60;
     sec=60;
+
     if(maxTime%60!=0){
         sec=maxTime%60;
     }else{
@@ -33,37 +35,51 @@ function initializeValues(){
 }
 
 
+//to handle remaining time to display
 function displayRemainingTime(){        
     if(sec==0){
         minutes=minutes-1;
         sec=59;
     }
     sec=sec-1;
-    span.innerText=`${minutes}:${sec}`;    
+    span.innerText=`${minutes}:${sec}`;   
 
 }
 
-
-
-
+//to keep count of increase in progress
 function increaseCounter(){    
         currTime++;     
         progress.value=progress.value+1;
         displayRemainingTime();
         if(currTime==maxTime){
            clearInterval(IntervalId);
+           input.value="";
            isnewStart=true;
         }      
 }
 
 
+//Event listeners
 start.addEventListener('click',()=>{
+    if(input.value.trim()=="" || input.value.trim()==0){
+        alert("Please enter valid value");
+    }
+    else{
     initializeValues();
    IntervalId=setInterval(increaseCounter,1000)
-   
+    }   
 })
 
 pause.addEventListener("click",()=>{
     clearInterval(IntervalId);
      isnewStart=false;
+})
+
+reset.addEventListener("click",()=>{
+    clearInterval(IntervalId);
+    progress.value=0;
+    input.value="";
+    progress.removeAttribute(progress.max);
+    span.innerText="";
+
 })
